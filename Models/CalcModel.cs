5895b12875a9a8bc1e3f2075865cb7f9c524233d
Rhythmkad <91103424+Rhythmkad@users.noreply.github.com>
2022-04-21 22:11:32 -0700

    Add project files.

---
+++ Models/CalcModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorProjectCalc1.Models
+{
+    public class CalcModel
+    {
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = " Input1 should not be zero or negative")]
+        public double Input1 { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = " Enter only positive integer value in Input2")]
+        public double Input2 { get; set; }
+    }
+}
